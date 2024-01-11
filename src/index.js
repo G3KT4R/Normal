b0dfc98025d33798b1fa8596eb808a1abcf6255b
GermanPaul <german.kalmykov@mail.ru>
2024-01-11 14:45:46 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import "./index.css";
 const num = 5;
 console.log(num);
+
+const f = (num) => {
+  if (num === 10) {
+    console.log("Равно 10");
+    return;
+  } else {
+    console.log("Не равно 10");
+  }
+};
