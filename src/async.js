862674f3dbc3c86bf044bcbd48fe5b6f179122b9
GermanPaul <german.kalmykov@mail.ru>
2024-01-15 10:44:56 +0300

    async homework

--- src/async.js
+++ src/async.js
@@ -1,6 +1,52 @@
-import './index.css';
+// import "./index.css";
 
-let i = 0;
-while (i < 100) {
-  i += 1;
-}
+// fetch("https://jsonplaceholder.typicode.com/posts/1/comments")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+const f1 = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/posts/1/comments',
+  );
+  const data = await res.json();
+  console.log('-------Id div 2 = 0-------');
+  console.log(data);
+  await data.forEach((element) => {
+    if (element.id % 2 === 0) {
+      console.log(`id: ${element.id}\nname: ${element.name}`);
+    }
+  });
+};
+
+f1();
+
+const f2 = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/albums/1/photos',
+  );
+  const data = await res.json();
+  console.log('-------Title of photos with A-------');
+  await data.forEach((element) => {
+    if (element.title.indexOf('a') === 0) {
+      console.log(`id: ${element.id}\nname: ${element.title}`);
+    }
+  });
+};
+
+f2();
+
+const f3 = async () => {
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users/1/albums',
+  );
+  const data = await res.json();
+  console.log('-------Id > 5-------');
+  console.log(data);
+  await data.forEach((element) => {
+    if (element.id > 5) {
+      console.log(`id: ${element.id}\nname: ${element.title}`);
+    }
+  });
+};
+
+f3();
