13959503c760ba7a489106c9a35e72991822a5d4
G3KT4R <german.kalmykov@mail.ru>
2024-01-23 22:46:07 +0300

    Typescript for array

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-var num = 4;
+console.log("--------First exercise--------");
+var map = function (e) {
+  return e;
+};
+var arr = [100, 150, 200, 250];
+var newArr = arr.map(function (e) {
+  return e + " р";
+});
+console.log(newArr);
+console.log("--------END--------\n");
+console.log("--------Second exercise--------");
+var users = [
+  ["alex", 32],
+  ["tomas", 17],
+  ["olga", 14],
+  ["andre", 24],
+];
+users.forEach(function (element) {
+  if (element[1] > 18) console.log(element);
+});
+// oldUsers = users.filter((element) => element[1] > 18);
+// oldUsers.forEach((element) => {
+//   console.log(element);
+// });
+console.log("--------END--------\n");
+console.log("--------Third exercise--------");
+var products = [
+  { title: "пицца", price: 200 },
+  { title: "баранина", price: 300 },
+  { title: "креветки", price: 400 },
+];
+var sum = products.reduce(function (acc, product) {
+  return acc + product.price;
+}, 0);
+console.log("Sum = ".concat(sum));
+console.log("--------END--------");
--- main.ts
+++ main.ts
@@ -1,1 +1,43 @@
-let num : number = 4;
+console.log("--------First exercise--------");
+const map = <T>(e: T): T => {
+  return e;
+};
+const arr: number[] = [100, 150, 200, 250];
+const newArr: string[] = arr.map((e) => e + " р");
+console.log(newArr);
+console.log("--------END--------\n");
+
+console.log("--------Second exercise--------");
+const users: [
+  [string, number],
+  [string, number],
+  [string, number],
+  [string, number],
+] = [
+  ["alex", 32],
+  ["tomas", 17],
+  ["olga", 14],
+  ["andre", 24],
+];
+users.forEach((element) => {
+  if (element[1] > 18) console.log(element);
+});
+
+// oldUsers = users.filter((element) => element[1] > 18);
+// oldUsers.forEach((element) => {
+//   console.log(element);
+// });
+console.log("--------END--------\n");
+
+console.log("--------Third exercise--------");
+
+const products = [
+  { title: "пицца", price: 200 },
+  { title: "баранина", price: 300 },
+  { title: "креветки", price: 400 },
+];
+const sum: number = products.reduce((acc, product) => {
+  return acc + product.price;
+}, 0);
+console.log(`Sum = ${sum}`);
+console.log("--------END--------");
