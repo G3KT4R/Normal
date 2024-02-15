d58ed709efc40231575ffcdd23e10545ee2f2c79
G3KT4R <german.kalmykov@mail.ru>
2024-02-15 23:05:41 +0300

    redux 34:03

---
+++ test-redux/src/productsSlice/index.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  list: [],
+};
+
+const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {
+    addProducts: (state, action) => {
+      state.list = action.payload;
+    },
+  },
+});
+
+export const { addProducts } = productsSlice.actions;
+export default productsSlice.reducer;
